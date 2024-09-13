d5631adbbb13d8cab26672b73cf371a97238d200
zpcore <zpcore@gmail.com>
2024-09-13 16:21:47 -0700

    nit

--- Training/TPU-v5p/Diffusion-2.1-PyTorch/train.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-python /home/diffusers/examples/text_to_image/train_text_to_image_xla.py \
---pretrained_model_name_or_path=stabilityai/stable-diffusion-2-base \
---dataset_name=$DATASET_NAME --resolution=$RESOLUTION --center_crop --random_flip \
---train_batch_size=$PER_HOST_BATCH_SIZE --max_train_steps=$TRAIN_STEPS \
---learning_rate=1e-06 --mixed_precision=bf16 --profile_duration=$PROFILE_DURATION \
---output_dir=$OUTPUT_DIR --dataloader_num_workers=4 \
---loader_prefetch_size=4 --device_prefetch_size=4
